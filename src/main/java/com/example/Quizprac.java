e8e1554a73d691ff4a1d9e6a0ad82fedea8fb658
Humaira Kauser <154069699+HumairaKauser76@users.noreply.github.com>
2025-12-01 22:51:11 +0500

    Update Quizprac.java

--- src/main/java/com/example/Quizprac.java
+++ src/main/java/com/example/Quizprac.java
@@ -1,6 +1,6 @@
 package com.example;
 public class Quizprac{
     public static void main(String[] args){
-        System.out.println("Hello from Jenkins v2!");\
+        System.out.println("Hello from Jenkins v2!");
     }
 }
